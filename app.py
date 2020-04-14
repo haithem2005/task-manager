d480c066a4d9ea1125acd8162deb2d7039db093e
haithem2005 <h.elhessawy@gmail.com>
2020-04-14 13:38:37 +0000

    setting local variable

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-9dy5k.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 
